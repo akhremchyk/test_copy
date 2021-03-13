c70dd7f2d01f2e1e0c319da352f2027b56bfad9c
Sttrom <gusev.n2002@gmail.com>
2021-03-13 22:32:35 +0300

    Added printField() and initialFill() to Field class. Started gameBody() in Main class.

--- src/com/ttt/Field.java
+++ src/com/ttt/Field.java
@@ -1,4 +1,34 @@
 package com.ttt;
 
+import java.util.Arrays;
+
 public class Field {
+
+    char[][] cell = new char[3][3];
+
+    public void Field()
+    {
+        this.initialFill();
+    }
+
+    public void printField()
+    {
+        System.out.println(
+                                "\t " + cell[0][0] + " | " + cell[0][1] + " | " + cell[0][2] + " \n" +
+                                "\t---|---|---\n" +
+                                "\t " + cell[1][0] + " | " + cell[1][1] + " | " + cell[1][2] + " \n" +
+                                "\t---|---|---\n" +
+                                "\t " + cell[2][0] + " | " + cell[2][1] + " | " + cell[2][2] + " \n\n");
+    }
+
+    public void initialFill() {
+
+        char cell_num = '1';
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++) {
+                cell[i][j] = cell_num;
+                cell_num ++;
+            }
+        }
+    }
 }
--- src/com/ttt/Main.java
+++ src/com/ttt/Main.java
@@ -1,20 +1,87 @@
 package com.ttt;
 
 import java.io.Console;
+import java.util.Scanner;
 
 public class Main {
 
+    private static Field field = new Field();
+    private static Scanner scan = new Scanner(System.in);
+    private static boolean bot = false;
+
     public static void main(String[] args) {
 
-        System.out.println(
-                "\t 1 | 2 | 3 \n" +
-                "\t---|---|---\n" +
-                "\t 4 | 5 | 6 \n" +
-                "\t---|---|---\n" +
-                "\t 7 | 8 | 9 \n\n");
-
+        field.printField();
         System.out.println("To fill a cell enter its number\n");
 
-        
+        gameBody();
+
     }
+
+    private static void gameBody() {
+        Character player = 'X';
+
+
+        // cls
+
+        field.printField();
+
+        System.out.println("1. Player vs Computer");
+        System.out.println("2. Player vs Player");
+
+
+        for (; ; ) {
+            int sw = 0;
+            if (scan.hasNextInt()) {
+                sw = scan.nextInt(); //switch
+            } else {
+                // throw 1; !!!!!!!!!!!!!!!!!!!!!!!!!
+            }
+
+            if (sw == 1) {
+                bot = true;
+                break;
+            } else if (sw == 2) {
+                bot = false;
+                break;
+            }
+        }
+
+        field.printField();
+        System.out.println("Player X's turn");
+
+//        for (;;)
+//        {
+//             try
+//            {
+//                //Если бот включен и его ход
+//                if (bot && player == 'O')
+//                    botTurn(player);
+//
+//                else
+//                    turn(player);
+//
+//                if (check_end() != 'c')
+//                    break;
+//
+//                if ((bot) || (bot && player == 'X'))
+//                    cout << "Player " << player << "'s turn" << endl;
+//            }
+//            catch(int err)
+//            {
+//                //Не выводит ошибки при ходе бота
+//                if (err == 1 && ( (!bot) ||
+//                        (bot && player == 'X') ) )
+//                {
+//                    cout << "The cell is already occupied" << endl;
+//                }
+//                if (err == 2 && ( (!bot) ||
+//                        (bot && player == 'X') ) )
+//                {
+//                    cout << "Enter a valid cell number" << endl;
+//                }
+//            }
+//        }
+    }
+
 }
